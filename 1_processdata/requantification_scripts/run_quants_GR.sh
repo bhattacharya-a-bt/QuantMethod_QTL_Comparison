11efb943d935c1a910857c456f4a9b13eaeba15c
staylorhead <47358928+staylorhead@users.noreply.github.com>
2026-06-29 15:37:57 -0500

    Delete 1_processdata directory

--- 1_processdata/requantification_scripts/run_quants_GR.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tissue="$1"
-user="$2"
-
-DIR_SCRIPTS=/rsrch5/home/epi/bhattacharya_lab/data/GTEx_v8/requants/Scripts
-
-bsub -env TISSUE=${tissue},USER=${user} < ${DIR_SCRIPTS}/run_quants_GR.lsf
