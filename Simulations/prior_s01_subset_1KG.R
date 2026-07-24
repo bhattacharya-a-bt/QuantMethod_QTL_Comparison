170e9ccd5662dcd93ee5773140abbe7cb708322a
staylorhead <47358928+staylorhead@users.noreply.github.com>
2026-07-24 11:40:47 -0500

    Update prior_s01_subset_1KG.R

--- Simulations/prior_s01_subset_1KG.R
+++ Simulations/prior_s01_subset_1KG.R
@@ -1,9 +1,36 @@
-pass = 2
-n = 500
+#!/usr/bin/env Rscript
+
+###################################################################
+# edit these paths and parameters before running
+###################################################################
+
+## ---- static parameters ----
+pass <- 2 # run apss
+n <- 500 # number of subjects to extract from 1KG for simulation
+seed <- 12345
+pop <- "EUR" # 1KG population to simulation from
+
+## ---- input reference file ----
+geno_file <- "/rsrch5/home/epi/bhattacharya_lab/data/GenomicReferences/ldref/1KG/all_hg38.psam"
+
+## ---- project / working directory ----
+# project directory base; the pass number is appended automatically.
+# kgp3.RData is read from <proj_dir>/files_for_analysis/, from kgp R package 
+proj_base_dir <- "/rsrch5/scratch/epi/sthead/GTEx_gencode_comp"
+
+## ---- output directory ----
+# output directory base; the pass number is appended automatically
+out_base_dir <- "/rsrch5/scratch/epi/sthead/GTEx_gencode_comp"
 
 ####################################################################################
+# derived paths (do not edit below unless changing structure)
+####################################################################################
+proj_dir <- paste0(proj_base_dir, "/pass", pass)
+directory <- paste0(out_base_dir, "/pass", pass, "/files_for_analysis")
 
-directory <- paste0("/rsrch5/scratch/epi/sthead/isoqtl_quant_error/pass",pass,"/files_for_analysis")
+####################################################################################
+# setup output directory
+####################################################################################
 
 # check if the directory doesn't exist
 if (!file.exists(directory)) {
@@ -14,12 +41,14 @@ if (!file.exists(directory)) {
   cat("Directory already exists:", directory, "\n")
 }
 
-geno_dir = "/rsrch5/home/epi/bhattacharya_lab/data/GenomicReferences/ldref/1KG"
-proj_dir = paste0("/rsrch5/scratch/epi/sthead/GTEx_gencode_comp/pass",pass)
 setwd(proj_dir)
 
-dat <- read.csv(paste0(geno_dir,"/all_hg38.psam"),sep="\t")
-eur <- dat[dat$SuperPop=="EUR",]
+####################################################################################
+# select EUR samples from 1KG
+####################################################################################
+
+dat <- read.csv(geno_file, sep = "\t")
+eur <- dat[dat$SuperPop == pop, ]
 table(eur$Population)
 dim(eur) # 633x6
 
@@ -28,13 +57,13 @@ dim(eur) # 633x6
 load("files_for_analysis/kgp3.RData")
 dim(kgp3) # 2504 samples sequenced for the phase 3 release
 
-eur <- eur[eur$X.IID %in% kgp3$id,]
+eur <- eur[eur$X.IID %in% kgp3$id, ]
 dim(eur) # 503x6
 
-# randomly select 500
-set.seed(12345)
-sel <- sample(1:nrow(eur),n)
-samp <- data.frame(ID=eur[sel,1])
+# randomly select n
+set.seed(seed)
+sel <- sample(1:nrow(eur), n)
+samp <- data.frame(ID = eur[sel, 1])
 
-write.table(samp,file="files_for_analysis/1kg_eur_500_sample_ids",
-            sep="\t",row.names = F,col.names = F,quote=F)
+write.table(samp, file = "files_for_analysis/1kg_eur_500_sample_ids",
+            sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
