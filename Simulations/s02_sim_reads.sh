ef4522d1c607271893ecedd3ee4d7171691b1819
staylorhead <47358928+staylorhead@users.noreply.github.com>
2026-07-27 15:18:02 -0500

    Update s02_sim_reads.sh

--- Simulations/s02_sim_reads.sh
+++ Simulations/s02_sim_reads.sh
@@ -5,9 +5,11 @@ module load R/4.3.1
 
 PASS=$1
 PARAM_ROW_READS=$2
+BASE_DIR=$3
+TXOME_FA=$4
 
 # run the simulationR script with the job index as an argument
-Rscript /rsrch5/scratch/epi/sthead/GTEx_gencode_comp/pass${PASS}/scripts/s02_sim_reads.R ${LSB_JOBINDEX} ${PASS} ${PARAM_ROW_READS}
+Rscript /rsrch5/scratch/epi/sthead/GTEx_gencode_comp/pass${PASS}/scripts/s02_sim_reads.R ${LSB_JOBINDEX} ${PASS} ${PARAM_ROW_READS} ${BASE_DIR} ${TXOME_FA}
 
 # unload modules
 module unload R
